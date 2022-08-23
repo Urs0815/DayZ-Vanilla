f5d720fcb0a525a4bca7d753f9e54e617c390a63
Urs <urs0815@web.de>
2022-08-24 01:04:19 +0200

    DayZ 1.18

---
+++ DZ/vehicles/proxies/config.cpp
@@ -0,0 +1,10 @@
+class CfgPatches
+{
+	class DZ_Vehicles_Wheeled// prolly same name for pbopro to say required addons
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]={"DZ_Vehicles_Parts","DZ_Data"};
+	};
+};
