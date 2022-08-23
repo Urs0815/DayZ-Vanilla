886925a0a942313f4f79347f342b9d8279295e2f
Urs <urs0815@web.de>
2022-08-24 01:39:22 +0200

    DayZ 1.18

---
+++ DZ/animals/animconfig/config.cpp
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////
+//DeRap: animconfig\config.bin
+//Produced from mikero's Dos Tools Dll version 8.84
+//https://mikero.bytex.digital/Downloads
+//'now' is Tue Aug 23 21:38:15 2022 : 'file' last modified on Mon Jan 24 09:23:41 2022
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+class CfgPatches
+{
+	class DZ_Animals_AnimConfig
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"DZ_Animals"};
+	};
+};
