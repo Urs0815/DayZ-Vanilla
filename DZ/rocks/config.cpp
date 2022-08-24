5de82bac2f847e792633d31f43d72a3b5c30e600
Urs <urs0815@web.de>
2022-08-24 02:04:04 +0200

    DayZ 1.18

---
+++ DZ/rocks/config.cpp
@@ -0,0 +1,57 @@
+////////////////////////////////////////////////////////////////////
+//DeRap: config.bin
+//Produced from mikero's Dos Tools Dll version 8.84
+//https://mikero.bytex.digital/Downloads
+//'now' is Tue Aug 23 21:41:11 2022 : 'file' last modified on Tue Mar 01 13:17:06 2022
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+class CfgPatches
+{
+	class DZ_Rocks
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"DZ_Data"};
+	};
+};
+class CfgNonAIVehicles
+{
+	class StaticObject;
+	class Static_stone1: StaticObject{};
+	class Static_stone2: StaticObject{};
+	class Static_stone3: StaticObject{};
+	class Static_stone4: StaticObject{};
+	class Static_stone5: StaticObject{};
+	class Static_rock_apart1: StaticObject{};
+	class Static_rock_apart2: StaticObject{};
+	class Static_rock_bright_apart1: StaticObject{};
+	class Static_rock_bright_apart2: StaticObject{};
+	class Static_rock_bright_monolith1: StaticObject{};
+	class Static_rock_bright_monolith2: StaticObject{};
+	class Static_rock_bright_monolith3: StaticObject{};
+	class Static_rock_bright_monolith4: StaticObject{};
+	class Static_rock_bright_spike1: StaticObject{};
+	class Static_rock_bright_spike2: StaticObject{};
+	class Static_rock_bright_spike3: StaticObject{};
+	class Static_rock_bright_wallh1: StaticObject{};
+	class Static_rock_bright_wallh2: StaticObject{};
+	class Static_rock_bright_wallh3: StaticObject{};
+	class Static_rock_bright_wallv: StaticObject{};
+	class Static_rock_monolith1: StaticObject{};
+	class Static_rock_monolith2: StaticObject{};
+	class Static_rock_monolith3: StaticObject{};
+	class Static_rock_monolith4: StaticObject{};
+	class Static_rock_spike1: StaticObject{};
+	class Static_rock_spike2: StaticObject{};
+	class Static_rock_spike3: StaticObject{};
+	class Static_rock_wallh1: StaticObject{};
+	class Static_rock_wallh2: StaticObject{};
+	class Static_rock_wallh3: StaticObject{};
+	class Static_rock_wallh4: StaticObject{};
+	class Static_rock_wallh5: StaticObject{};
+	class Static_rock_wallh6: StaticObject{};
+	class Static_rock_wallv: StaticObject{};
+};
---
+++ DZ/server/config.cpp
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////
+//DeRap: config.bin
+//Produced from mikero's Dos Tools Dll version 8.84
+//https://mikero.bytex.digital/Downloads
+//'now' is Tue Aug 23 21:41:14 2022 : 'file' last modified on Thu Jan 23 00:11:20 2020
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+class CfgPatches
+{
+	class DZ_Server_Data
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {};
+	};
+};
