0ca0f40da1adde1036708ee97ce843d47eae5533
Urs <urs0815@web.de>
2022-08-24 12:38:40 +0200

    DayZ 1.18

---
+++ DZ/structures/signs/config.cpp
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////
+//DeRap: config.bin
+//Produced from mikero's Dos Tools Dll version 8.84
+//https://mikero.bytex.digital/Downloads
+//'now' is Tue Aug 23 21:45:02 2022 : 'file' last modified on Tue Jun 21 17:55:18 2022
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+class CfgPatches
+{
+	class DZ_Structures_Signs
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"DZ_Data"};
+	};
+};
