43a9ca1020f344d545429686601db8fe26957890
Urs <urs0815@web.de>
2022-08-24 11:56:17 +0200

    DayZ 1.18

---
+++ DZ/structures/data/config.cpp
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////
+//DeRap: config.bin
+//Produced from mikero's Dos Tools Dll version 8.84
+//https://mikero.bytex.digital/Downloads
+//'now' is Tue Aug 23 21:42:55 2022 : 'file' last modified on Thu Dec 09 10:39:58 2021
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+//(8 Enums)
+enum {
+	destructengine = 2,
+	destructdefault = 6,
+	destructwreck = 7,
+	destructtree = 3,
+	destructtent = 4,
+	destructno = 0,
+	destructman = 5,
+	destructbuilding = 1
+};
+
+class CfgPatches
+{
+	class DZ_Structures
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"DZ_Data"};
+	};
+};
