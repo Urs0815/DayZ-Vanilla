9e0b19ff75a2ca74fae4f8c58f3934451231fef6
Urs <urs0815@web.de>
2022-08-24 02:13:47 +0200

    DayZ 1.18

---
+++ DZ/water_bliss/config.cpp
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////
+//DeRap: config.bin
+//Produced from mikero's Dos Tools Dll version 8.84
+//https://mikero.bytex.digital/Downloads
+//'now' is Tue Aug 23 21:49:48 2022 : 'file' last modified on Fri May 15 09:18:15 2020
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+//(8 Enums)
+enum {
+	destructengine = 2,
+	destructdefault = 6,
+	destructwreck = 7,
+	destructtree = 3,
+	destructtent = 4,
+	destructno = 0,
+	destructman = 5,
+	destructbuilding = 1
+};
+
+class CfgPatches
+{
+	class DZ_Water_Bliss
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"DZ_Data"};
+	};
+};
